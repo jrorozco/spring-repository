2c835b14f4dfa395a1b25350a673d5ad4200c7de
jrorozco <desarrollador1@index.org.mx>
2020-08-07 01:16:25 -0500

    prueba para subir al git

---
+++ sping-boot-di/src/main/java/com/mx/jros/di/app/SpingBootDiApplication.java
@@ -0,0 +1,13 @@
+package com.mx.jros.di.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootDiApplication.class, args);
+	}
+
+}
---
+++ sping-boot-di/src/test/java/com/mx/jros/di/app/SpingBootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.jros.di.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBootDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
